04d24d9e8fee12f805024c9b7ba0544d6ec21acc
alex-fossa-204 <shift.kite11@yandex.ru>
2023-08-10 15:49:23 +0300

    [develop]: очистил Main

--- src/main/java/com/example/demo/interview/Main.java
+++ src/main/java/com/example/demo/interview/Main.java
@@ -1,46 +1,11 @@
 package com.example.demo.interview;
 
-import com.example.demo.stubFunctionality.entity.Role;
-import com.example.demo.stubFunctionality.entity.User;
+import lombok.extern.slf4j.Slf4j;
 
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
 
+@Slf4j
 public class Main {
-    public static void main(String[] args) {
-        var users = List.of(
-                User.of("misterSmith14", "Jerry", "Smith", LocalDateTime.now(), List.of(Role.of("USER", List.of("READ", "CREATE", "UPDATE")))),
-                User.of("rickTheBest", "Rick", "Sunches", LocalDateTime.now(), List.of(Role.of("ADMIN", List.of("READ", "CREATE", "UPDATE", "DELETE")))),
-                User.of("mortyEvil", "Morty", "Smith Evil", LocalDateTime.now(), List.of(Role.of("ADMIN", List.of("READ", "CREATE", "UPDATE", "DELETE"))))
-        );
-
-        var usernamesToPrivileges = users.stream()
-                .collect(Collectors.toMap(
-                        User::getUsername,
-                        user -> user.getRoles().stream()
-                                .flatMap(role -> role.getPrivileges().stream())
-                                .collect(Collectors.toList()),
-                        (key, value) -> value,
-                        ConcurrentHashMap::new
-                ));
-
-        usernamesToPrivileges.entrySet().forEach(System.out::println);
-
-        var usernamesToPrivilegesConcurrentHashMap = users.stream()
-                .collect(Collectors.toMap(
-                        User::getUsername,
-                        user -> user.getRoles().stream()
-                                .flatMap(role -> role.getPrivileges().stream())
-                                .toList(),
-                        (key, value) -> value,
-                        HashMap::new
-                ));
-        usernamesToPrivilegesConcurrentHashMap.entrySet()
-                .forEach(System.out::println);
-
+    public static void main(String[] args) throws InterruptedException {
 
     }
 }
